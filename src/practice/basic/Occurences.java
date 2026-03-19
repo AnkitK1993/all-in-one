162aa971fa4c5f015ced3b1eeff3bebdcb7c1620
Ankit Konchady <ankit.konchady@neebal.com>
2026-03-19 15:07:27 +0530

    [CHANGES]--Add BuyOrSellStock and TwoSum implementations; refactor Occurences class

--- src/practice/basic/Occurences.java
+++ src/practice/basic/Occurences.java
@@ -6,9 +6,12 @@ import java.util.stream.Collectors;
 public class Occurences {
     public static void main(String[] args) {
         String s = "mediterranean";
-        Map<Character, Long> map = s.chars()
-                .mapToObj(c -> (char) c)
-                .collect(Collectors.groupingBy(c -> c, Collectors.counting()));
-        map.forEach((k, v) -> System.out.println("Key : " + k + " || value : " + v));
+        System.out.println(test());
+    }
+
+    static Map<Character,Long> test(){
+        return "ANKITKONCHADY".chars()
+                .mapToObj(c -> (char)c)
+                .collect(Collectors.groupingBy(c->c,Collectors.counting()));
     }
 }
