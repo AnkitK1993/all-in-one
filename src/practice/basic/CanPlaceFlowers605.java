594bfe5f058bbd48c1762041d29b4cf3665678f4
Ankit Konchady <ankit.konchady@neebal.com>
2026-03-24 19:24:33 +0530

    [Add]--Added can place flowers leetcode 605

---
+++ src/practice/basic/CanPlaceFlowers605.java
@@ -0,0 +1,27 @@
+package practice.basic;
+
+public class CanPlaceFlowers605 {
+    public static void main(String[] args) {
+        int[] input = {1,0,0,0,1};
+        int n = 1;
+        System.out.println(canPlaceFlowers(input,n));
+    }
+
+    public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int count = 0;
+        int length = flowerbed.length;
+        for (int i = 0; i < length; i++) {
+            if (flowerbed[i] == 0) {
+                boolean emptyLeft = (i == 0) || (flowerbed[i - 1] == 0);
+                boolean emptyRight = (i == length - 1) || (flowerbed[i + 1] == 0);
+                if (emptyLeft && emptyRight) {
+                    flowerbed[i] = 1;
+                    count++;
+                    if (count >= n) return true;
+                }
+            }
+        }
+        return count >= n;
+
+    }
+}
