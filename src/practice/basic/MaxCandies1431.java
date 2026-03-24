e4d89b566a6c7d716129e9dd83689b3bf9d9517f
Ankit Konchady <ankit.konchady@neebal.com>
2026-03-24 19:12:46 +0530

    [Add]--Added max candies leetcode 1431

---
+++ src/practice/basic/MaxCandies1431.java
@@ -0,0 +1,26 @@
+package practice.basic;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MaxCandies1431 {
+    public static void main(String[] args) {
+        int[] candies = {2, 3, 5, 1, 3};
+        int extraCandies = 3;
+        System.out.println(kidsWithCandies(candies, extraCandies));
+    }
+
+    private static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        int max = 0;
+        for (int candy : candies) {
+            max = Math.max(max, candy);
+        }
+
+        List<Boolean> result = new ArrayList<>();
+        for (int candy : candies) {
+            result.add(candy + extraCandies >= max);
+        }
+        return result;
+    }
+
+}
