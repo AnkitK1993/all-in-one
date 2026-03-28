fb1776213c1f2389deb72d1976913771b82432cb
Ankit Konchady <ankit.konchady@neebal.com>
2026-03-28 14:09:18 +0530

    [REFACTORING]--refactoring

--- src/practice/dsa/stringsAndArrays/ReverseVowels345.java
+++ src/practice/dsa/stringsAndArrays/ReverseVowels345.java
@@ -11,25 +11,25 @@ public class ReverseVowels345 {
         int left = 0;
         int right = s.length() - 1;
         boolean[] isVowel = new boolean[128];
-        for(char vowel : "aeiouAEIOU".toCharArray())
-            isVowel[vowel]=true;
+        for (char vowel : "aeiouAEIOU".toCharArray())
+            isVowel[vowel] = true;
 
-        while(left<right){
-            while(left<right && !isVowel[chars[left]])
+        while (left < right) {
+            while (left < right && !isVowel[chars[left]])
                 left++;
-            while(left<right && !isVowel[chars[right]])
+            while (left < right && !isVowel[chars[right]])
                 right--;
 
-            if(left<right){
+            if (left < right) {
                 char temp = chars[left];
-                chars[left]=chars[right];
-                chars[right]=temp;
+                chars[left] = chars[right];
+                chars[right] = temp;
 
                 left++;
                 right--;
 
             }
         }
-    return String.valueOf(chars);
+        return String.valueOf(chars);
     }
 }
